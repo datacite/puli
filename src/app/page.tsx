827a64d2811259768423770501d27a395db0e2e7
Bryceson Laing <bklaing2@gmail.com>
2025-10-20 15:18:28 -0500

    create horizontal bar chart for present values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HorizontalBarChart from "@/components/PresentHorizontalBarChart";
+
 export default function Home() {
   return (
     <main>
       <h1>DataCite Metadata Analytics Dashboard</h1>
+      <HorizontalBarChart />
     </main>
   );
 }
---
+++ src/components/PresentHorizontalBarChart.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import {
+  Bar,
+  BarChart,
+  LabelList,
+  type LabelProps,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { type ChartConfig, ChartContainer } from "@/components/ui/chart";
+
+const BAR_RADIUS = 5;
+
+export const description = "A bar chart with a custom label";
+
+const chartData = [
+  { category: "creatorName", present: 70 },
+  { category: "givenName", present: 60 },
+  { category: "familyName", present: 98 },
+  { category: "nameType", present: 100 },
+  { category: "nameIdentifier", present: 30 },
+  { category: "affiliation", present: 20 },
+].map((item) => ({ ...item, absent: 100 - item.present }));
+
+const chartConfig = {
+  present: {
+    label: "Present",
+    color: "var(--color-primary-light-blue)",
+  },
+  absent: {
+    label: "Absent",
+    color: "var(--color-primary-dark-blue)",
+  },
+} satisfies ChartConfig;
+
+export default function HorozontalBarChart() {
+  const barSize = 20;
+  const barGap = 30;
+  const containerHeight = chartData.length * (barSize + barGap) + 40;
+
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="min-w-full"
+      style={{ height: `${containerHeight}px` }}
+    >
+      <BarChart
+        accessibilityLayer
+        data={chartData}
+        layout="vertical"
+        margin={{
+          right: 50,
+        }}
+        barSize={barSize}
+        barCategoryGap={barGap}
+        barGap={barGap}
+      >
+        <YAxis dataKey="category" type="category" hide />
+        <XAxis dataKey="present" type="number" hide />
+        <Bar
+          dataKey="present"
+          fill="var(--color-present)"
+          background={{ fill: "var(--color-absent)", radius: BAR_RADIUS }}
+          radius={BAR_RADIUS}
+          stackId={1}
+        >
+          <LabelList dataKey="category" content={CategoryLabel} />
+        </Bar>
+        <Bar dataKey="absent" fill="#ffffff00" stackId={1}>
+          <LabelList
+            dataKey="present"
+            position="right"
+            fontSize={12}
+            formatter={asPercent}
+          />
+        </Bar>
+      </BarChart>
+    </ChartContainer>
+  );
+}
+
+function CategoryLabel(props: LabelProps) {
+  const { x, y, value } = props;
+  return (
+    <text x={x} y={Number(y) - 6} fontSize={12} textAnchor="start">
+      {value}
+    </text>
+  );
+}
+
+function asPercent(value: number) {
+  return `${value}%`;
+}
