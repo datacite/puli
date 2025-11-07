034f7c057ec14595d39e8663a5755405593f571e
Bryceson Laing <bklaing2@gmail.com>
2025-11-07 11:08:57 -0600

    style high impact badge

--- src/components/HighImpactBadge.tsx
+++ src/components/HighImpactBadge.tsx
@@ -1,3 +1,5 @@
+import { Badge } from "@/components/ui/badge";
+
 interface Props {
   show: boolean;
 }
@@ -6,5 +8,12 @@ export default function HighImpactBadge(props: Props) {
   const { show = true } = props;
 
   if (!show) return null;
-  return " (High Impact)";
+  return (
+    <Badge
+      variant="secondary"
+      className="ml-2 rounded-sm text-[0.8em] p-y-0 p-x-1"
+    >
+      HIGH IMPACT
+    </Badge>
+  );
 }
