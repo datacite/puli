22b0b370173184fe5c12c457899b0782f198bc7c
Bryceson Laing <bklaing2@gmail.com>
2026-01-05 16:34:14 -0600

    improve loading state for repository name

--- src/app/[clientId]/Header.tsx
+++ src/app/[clientId]/Header.tsx
@@ -5,8 +5,13 @@ import useOverview from "@/data/fetchOverview";
 export default function Header() {
   const { isPending, isError, data, error } = useOverview();
 
-  if (isPending) return "Loading...";
   if (isError) return `Error: ${error}`;
 
-  return <h2 className="text-4xl w-full">{data.name}</h2>;
+  const name = isPending ? "Repository Name" : data.name;
+
+  return (
+    <h2 className="text-4xl w-full" style={isPending ? { opacity: 0.2 } : {}}>
+      {name}
+    </h2>
+  );
 }
