598ee40762e2f092549435df95dae83f5a894416
Bryceson Laing <bklaing2@gmail.com>
2026-01-06 16:04:14 -0600

    style new DataCite headings to be used in place of `hx`

---
+++ src/components/datacite/Headings.tsx
@@ -0,0 +1,45 @@
+import { dmSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+
+export function H1({ className, ...props }: React.ComponentProps<"h1">) {
+  return <h1 className={cn("", className)} {...props} />;
+}
+
+export function H2({ className, ...props }: React.ComponentProps<"h2">) {
+  return (
+    <h2
+      className={cn("text-4xl font-normal", dmSans.className, className)}
+      {...props}
+    />
+  );
+}
+
+export function H3({ className, ...props }: React.ComponentProps<"h3">) {
+  return (
+    <h3
+      className={cn(
+        "text-2xl font-semibold text-muted-foreground",
+        dmSans.className,
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export function H4({ className, ...props }: React.ComponentProps<"h4">) {
+  return (
+    <h4
+      className={cn("text-card-title text-xl", dmSans.className, className)}
+      {...props}
+    />
+  );
+}
+
+export function H5({ className, ...props }: React.ComponentProps<"h5">) {
+  return <h5 className={cn("", className)} {...props} />;
+}
+
+export function H6({ className, ...props }: React.ComponentProps<"h6">) {
+  return <h6 className={cn("", className)} {...props} />;
+}
