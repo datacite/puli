329343b7b761630cfdf9229c5a1ace02ef0d863e
Bryceson Laing <bklaing2@gmail.com>
2026-01-06 15:58:08 -0600

    replace repository name loading text with `clientId`

--- src/app/[clientId]/Header.tsx
+++ src/app/[clientId]/Header.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import useOverview from "@/data/fetchOverview";
+import { useClientId } from "@/hooks";
 
 export default function Header() {
+  const clientId = useClientId();
   const { isPending, isError, data, error } = useOverview();
 
   if (isError) return `Error: ${error}`;
 
-  const name = isPending ? "Repository Name" : data.name;
+  const name = isPending ? clientId : data.name;
 
-  return (
-    <h2 className="text-4xl w-full" style={isPending ? { opacity: 0.2 } : {}}>
-      {name}
-    </h2>
-  );
+  return <h2 className="text-4xl w-full">{name}</h2>;
 }
