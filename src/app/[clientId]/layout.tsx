8872dfbec27e27a4149f477fe4508bc8b638a417
Bryceson Laing <bklaing2@gmail.com>
2026-01-05 23:25:45 -0600

    throw `notFound` if client does not exist

--- src/app/[clientId]/layout.tsx
+++ src/app/[clientId]/layout.tsx
@@ -1,12 +1,24 @@
+import { notFound } from "next/navigation";
 import ActionButtons from "@/components/ActionButtons";
 import Breadcrumbs, { type BreadcrumbData } from "@/components/Breadcrumbs";
+import { fetchDatacite } from "@/util";
 import Header from "./Header";
 
 export default async function Layout({
+  params,
   children,
 }: {
+  params: Promise<{ clientId: string }>;
   children: React.ReactNode;
 }) {
+  // Check if client exists
+  const { clientId } = await params;
+  const res = await fetchDatacite(`clients/${clientId}`, {
+    cache: "force-cache",
+  });
+  const json = await res.json();
+  if (!json.data) notFound();
+
   const pages: BreadcrumbData[] = [
     { title: "Home", href: "/" },
     { title: "Example", href: "/" },
