1130a40710b9987ad329d6708bc588b26f56fa70
Bryceson Laing <bklaing2@gmail.com>
2026-01-21 14:27:37 -0600

    add global fetching indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
+import LoadingIndicator from "@/components/LoadingIndicator";
 import { barlow } from "@/lib/fonts";
 import Header from "./Header";
 import Providers from "./Providers";
@@ -23,6 +24,7 @@ export default function RootLayout({
           <div className="flex flex-col gap-8 py-4 max-w-7xl mx-auto px-6">
             {children}
           </div>
+          <LoadingIndicator />
         </Providers>
         <Script id="feedback-button">
           {`(function(){window.onUsersnapCXLoad=function(e){e.init()};var e=document.createElement("script");e.defer=1,e.src="https://widget.usersnap.com/global/load/b4393e90-ec13-4338-b299-7b6f122b7de3?onload=onUsersnapCXLoad",document.getElementsByTagName("head")[0].appendChild(e)})();`}
