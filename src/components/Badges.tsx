cf91cda73d64f4b918128e37eb9c23a417ce41b6
Bryceson Laing <bklaing2@gmail.com>
2026-02-10 19:44:55 -0600

    add `ResourceBadge` to show `Resource` type (`client`, `direct_member`, `consortium_organization`, `consortium`)

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,21 +1,25 @@
 import { Badge, type BadgeProps } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 
-interface Props extends BadgeProps {
-  show: boolean;
+const DEFAULT_CLASS =
+  "ml-2 rounded-[40px] text-datacite-blue-dark bg-datacite-blue-light/20 p-y-0 p-x-1 border-none";
+
+export function ResourceBadge(props: { resource: { subtype: string } }) {
+  return (
+    <Badge variant="outline" className={DEFAULT_CLASS}>
+      {props.resource.subtype.replaceAll("_", " ")}
+    </Badge>
+  );
 }
 
-export function HighImpactBadge(props: Props) {
+export function HighImpactBadge(props: { show: boolean } & BadgeProps) {
   const { show = true, className, ...badgeProps } = props;
 
   if (!show) return null;
   return (
     <Badge
       variant="outline"
-      className={cn(
-        "ml-2 rounded-[40px] text-datacite-blue-dark bg-datacite-blue-light/20 text-[0.8em] p-y-0 p-x-1 border-none",
-        className,
-      )}
+      className={cn(DEFAULT_CLASS, "text-[0.8em]", className)}
       {...badgeProps}
     >
       HIGH IMPACT
