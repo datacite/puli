6d833d098de4c7276015d3fc5c5ff7b1c5eb609c
codycooperross <50597551+codycooperross@users.noreply.github.com>
2026-02-25 13:59:32 +0100

    Center and spacing for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Page({ searchParams }: PageProps<"/">) {
 
   return (
     <main className="flex flex-col gap-4 w-3xl mx-auto mb-50">
-      <H2>Search repositories and organizations</H2>
+      <H2 className="text-center my-4">Search repositories and organizations</H2>
       <SearchEntities query={queryString} />
       <DisplayEntities query={queryString} />
     </main>
