9f3025ac7f7ba7223f7dcaad7251f3b30def23ac
Bryceson Laing <bklaing2@gmail.com>
2026-03-17 21:47:34 -0500

    use secondary logo; fixes #25

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { H1 } from "@/components/datacite/Headings";
-import logo from "./DataCite-Logo.svg";
+import logo from "./DataCite-Logo.png";
 
 export default function Header() {
   return (
     <header className="py-4 px-6">
       <H1 className="w-max">
         <Link href="/" prefetch>
-          <Image src={logo} alt="DataCite logo" height={75} className="" />
+          <Image src={logo} alt="DataCite logo" height={50} className="" />
         </Link>
       </H1>
     </header>
